ecc1c37bb30e5951475aee5bea99ae89d5e1b7f2
Lucas Monteiro <68429768+LucasAlmeida9755@users.noreply.github.com>
2022-03-03 22:31:24 -0300

    Add files via upload

---
+++ commands/add.js
@@ -0,0 +1,44 @@
+const {
+  SlashCommandBuilder
+} = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('add')
+    .setDescription('Aguarde....')
+    .addUserOption(option =>
+      option.setName('target')
+      .setDescription('Membro para adicionar ao ticket')
+      .setRequired(true)),
+  async execute(interaction, client) {
+    const chan = client.channels.cache.get(interaction.channelId);
+    const user = interaction.options.getUser('target');
+
+    if (chan.name.includes('ticket')) {
+      chan.edit({
+        permissionOverwrites: [{
+          id: user,
+          allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+        },
+        {
+          id: interaction.guild.roles.everyone,
+          deny: ['VIEW_CHANNEL'],
+        },
+          {
+            id: client.config.roleSupport,
+            allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+          },
+      ],
+      }).then(async () => {
+        interaction.reply({
+          content: `<@${user.id}> foi adicionado ao ticket!`
+        });
+      });
+    } else {
+      interaction.reply({
+        content: 'Você não está em um ticket!',
+        ephemeral: true
+      });
+    };
+  },
+};
